53c3f6224a4392425c03b66b34140bb7ef0025c4
betelhem demsis <bettydemsis@gmail.com>
2024-09-28 21:20:44 +0300

    initial

--- movieapi/movieapi/settings.py
+++ movieapi/movieapi/settings.py
@@ -39,6 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'movies',
+    'reviews',
+    'accounts',
+    'recommendations'
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
