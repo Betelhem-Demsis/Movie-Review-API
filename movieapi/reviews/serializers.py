b9ec22806132b2387940cb5ff1448d6370396577
betelhem demsis <bettydemsis@gmail.com>
2024-10-10 18:50:33 +0300

    modified:serializers.py

--- movieapi/reviews/serializers.py
+++ movieapi/reviews/serializers.py
@@ -20,14 +20,12 @@ class ReviewSerializer(serializers.ModelSerializer):
     comments = CommentSerializer(many=True, read_only=True)
     likes = LikeSerializer(many=True, read_only=True)
     like_count = serializers.IntegerField(read_only=True)
-    movie_title = serializers.CharField(source='movie.title', read_only=True)
-    movie_year = serializers.CharField(source='movie.year', read_only=True)
 
     class Meta:
         model = Review
         fields = [
             'id', 'movie_title', 'review_content', 'rating', 
             'user', 'comments', 'likes', 'like_count', 
-            'created_at', 'movie_year', 'movie'
+            'created_at'
         ]
         read_only_fields = ['created_at', 'user', 'like_count']
